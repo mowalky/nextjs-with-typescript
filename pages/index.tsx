5a672ea6ee5b41ad3ee854f80e1d9dd37020decf
Mowalky <dev@mowalky.com>
2022-07-20 22:11:28 -0500

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,14 +41,14 @@ export default function ResponsiveGrid() {
   return (
     <>
       <AppBar />
-      <Box sx={{ marginTop: "80px" }}>
+      <Box sx={{ marginTop: "4rem", padding: "1rem" }}>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 2, sm: 8, md: 12 }}
         >
           {Array.from(Array(60)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={6} key={index}>
+            <Grid item xs={2} sm={4} md={2} key={index}>
               <Card>
                 <CardHeader
                   avatar={
@@ -61,8 +61,8 @@ export default function ResponsiveGrid() {
                       <MoreVertIcon />
                     </IconButton>
                   }
-                  title="Shrimp and Chorizo Paella"
-                  subheader="September 14, 2016"
+                  title="Activity Title"
+                  subheader="Activity For..."
                 />
                 <CardActions disableSpacing>
                   <IconButton aria-label="add to favorites">
@@ -81,36 +81,11 @@ export default function ResponsiveGrid() {
                   </ExpandMore>
                 </CardActions>
                 <Collapse in={expanded} timeout="auto" unmountOnExit>
-                  <CardContent>
+                  <CardContent sx={{ backgroundColor: "rgb(0 0 0 / 5%)" }}>
                     <Typography paragraph>Method:</Typography>
                     <Typography paragraph>
                       Heat 1/2 cup of the broth in a pot until simmering, add
                       saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                      Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                      skillet over medium-high heat. Add chicken, shrimp and
-                      chorizo, and cook, stirring occasionally until lightly
-                      browned, 6 to 8 minutes. Transfer shrimp to a large plate
-                      and set aside, leaving chicken and chorizo in the pan. Add
-                      pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                      pepper, and cook, stirring often until thickened and
-                      fragrant, about 10 minutes. Add saffron broth and
-                      remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                      Add rice and stir very gently to distribute. Top with
-                      artichokes and peppers, and cook without stirring, until
-                      most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                      heat to medium-low, add reserved shrimp and mussels,
-                      tucking them down into the rice, and cook again without
-                      stirring, until mussels have opened and rice is just
-                      tender, 5 to 7 minutes more. (Discard any mussels that
-                      don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                      Set aside off of the heat to let rest for 10 minutes, and
-                      then serve.
                     </Typography>
                   </CardContent>
                 </Collapse>
