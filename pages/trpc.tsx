28d1abc82f8d72130d8f27db8246423ccfa68270
Mowalky <dev@mowalky.com>
2022-07-29 06:57:11 -0500

    data load

--- pages/trpc.tsx
+++ pages/trpc.tsx
@@ -3,7 +3,18 @@ import { useState } from "react";
 const TestPage: React.FC<{}> = () => {
   const [data, setData] = useState<null | { message: string }>(null);
 
-  return <div>test</div>;
+  const handleDataLoad = async () => {
+    const load = await fetch("/api/hello");
+    const data = await load.json();
+    setData(data);
+  };
+
+  return (
+    <>
+      <button onClick={handleDataLoad}>Load Data</button>
+      {!!data && <pre>Data: {data.message}</pre>}
+    </>
+  );
 };
 
 export default TestPage;
