6530e8434721d0febbbb242c6ee33e7cc5d674d6
Mowalky <dev@mowalky.com>
2022-07-28 21:34:40 -0500

    updated

--- pages/delay.tsx
+++ pages/delay.tsx
@@ -1,26 +1,31 @@
 import { useState } from "react";
 
-const ErrorMessage = () => <div>Something went wrong. Please try again.</div>;
+const ErrorMessage = () => (
+  <div style={{ display: "flex", justifyContent: "center", color: "red" }}>
+    Something went wrong. Please try saving again.
+  </div>
+);
 
 const DelayPage = () => {
   let [isSaving, setIsSaving] = useState(false);
   let [isError, setIsError] = useState(false);
-  let [users, setUsers] = useState([]);
+  let [posts, setPosts] = useState([]);
 
   async function handleSubmit() {
+    setPosts([]);
     setIsSaving(true);
     setIsError(false);
+
     try {
-      console.time("save");
       let [res]: any = await Promise.allSettled([
-        fetch("https://jsonplaceholder.typicode.com/users"),
+        fetch("https://jsonplaceholder.typicode.com/posts"),
         new Promise((resolve) => setTimeout(resolve, 800)),
       ]);
+
       const APIResponse: Response = res.value;
       let results = await APIResponse.json();
-      console.log(results);
-      setUsers(results);
-      console.timeEnd("save");
+
+      setPosts(results);
       setIsSaving(false);
     } catch (e) {
       setIsSaving(false);
@@ -33,11 +38,19 @@ const DelayPage = () => {
     <div>
       {!isSaving && <button onClick={handleSubmit}>save</button>}
       {isSaving && <button disabled>saving...</button>}
-      {isError && <ErrorMessage />}
       <hr />
+      {isError && <ErrorMessage />}
       <ul>
-        {users.map((user) => {
-          return <li>{user.name}</li>;
+        {posts.map((post: { title: string; body: string }) => {
+          return (
+            <li>
+              {post.title}
+              <br />
+              <em>
+                <small>{post.body}</small>
+              </em>
+            </li>
+          );
         })}
       </ul>
     </div>
