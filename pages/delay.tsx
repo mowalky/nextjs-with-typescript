eab04069f3fbf759921b937f4aff4ec4e56f850d
Mowalky <dev@mowalky.com>
2022-07-28 21:27:54 -0500

    user list

--- pages/delay.tsx
+++ pages/delay.tsx
@@ -5,19 +5,21 @@ const ErrorMessage = () => <div>Something went wrong. Please try again.</div>;
 const DelayPage = () => {
   let [isSaving, setIsSaving] = useState(false);
   let [isError, setIsError] = useState(false);
+  let [users, setUsers] = useState([]);
 
   async function handleSubmit() {
     setIsSaving(true);
     setIsError(false);
     try {
       console.time("save");
-      let [res] = await Promise.allSettled([
+      let [res]: any = await Promise.allSettled([
         fetch("https://jsonplaceholder.typicode.com/users"),
         new Promise((resolve) => setTimeout(resolve, 800)),
       ]);
       const APIResponse: Response = res.value;
       let results = await APIResponse.json();
       console.log(results);
+      setUsers(results);
       console.timeEnd("save");
       setIsSaving(false);
     } catch (e) {
@@ -32,6 +34,12 @@ const DelayPage = () => {
       {!isSaving && <button onClick={handleSubmit}>save</button>}
       {isSaving && <button disabled>saving...</button>}
       {isError && <ErrorMessage />}
+      <hr />
+      <ul>
+        {users.map((user) => {
+          return <li>{user.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
