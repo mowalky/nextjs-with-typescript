40d2e405aed65c54f68db3ed1630077787e75957
Mowalky <dev@mowalky.com>
2022-08-11 21:54:06 -0500

    updated react-query

--- pages/react-query.tsx
+++ pages/react-query.tsx
@@ -6,9 +6,33 @@ import {
 
 const queryClient = new QueryClient();
 
+const Example = (): any => {
+  const { isLoading, error, data } = useQuery(["repoData"], () =>
+    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+      (res) => res.json()
+    )
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error instanceof Error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+};
+
 const ReactQuery = () => {
   return (
-    <QueryClientProvider client={queryClient}>ReactQuery</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   );
 };
 
